594ef36d0389a586df6be1a2be14b66387f68254
LollipopFt <62802897+LollipopFt@users.noreply.github.com>
2021-12-14 14:26:23 +0800

    Initial commit
    initial test of rustc lint
    
    added name.
    
    hope that it works for rust enhanced.
    
    trying to make it work.
    
    hoping that this would work.
    
    previous commit actually fixed something, let's hope this one fixes regex problem.
    
    hope this works.
    
    removed unnecessary thing.
    
    fixed minor error.
    
    added filepath.
    
    i gave up.
    
    rustc somehow cannot work, trying to fix it.
    
    added filepath.
    
    made the string more comprehensive.
    
    tried to fix it as much as i could.
    
    used suggestions from github.
    
    trying to resolve stderr.
    
    using correct module.

---
+++ linter.py
@@ -0,0 +1,12 @@
+from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+
+
+class Rustc(Linter):
+    cmd = ('rustc', '--error-format=json', '${file}')
+    regex = r'''^{"message":"(?P<message>(?!could not emit MIR).*)","code":{"code":"(?P<code>.*)","explanation":.*},"level":"(?P<error_type>.*?)","spans":\[{.*"line_start":(?P<line>[0-9]+),"line_end":(?P<end_line>[0-9]+),"column_start":(?P<col>[0-9]+),"column_end":(?P<end_col>[0-9]+),.*"text":\[{"text":"(?P<near>.*)","highlight_start".*$'''
+    multiline = True
+    defaults = {
+        'selector': 'source.rust'
+    }
+    error_stream = util.STREAM_STDERR
+    name = 'rustc'

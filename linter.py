09ed539620ce6995c19c3eae125865c34d382528
LollipopFt <lollipopz1426@gmail.com>
2021-12-17 15:22:13 +0800

    initial attempt of using json.
    trying to get json working.
    
    forgot a semicolon.
    
    used a proper for statement.
    
    use .split() to split lines.
    
    test if error is fixed.
    
    hopefully python can parse this.
    
    debug.
    
    revert debug: works fine.

--- linter.py
+++ linter.py
@@ -1,12 +1,28 @@
-from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
-
+from SublimeLinter.lint import Linter, LintMatch, STREAM_STDERR  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+import json
 
 class Rustc(Linter):
     cmd = ('rustc', '--error-format=json', '${file}')
-    regex = r'''^{"message":"(?P<message>(?!could not emit MIR).*)","code":{"code":"(?P<code>.*)","explanation":.*},"level":"(?P<error_type>.*?)","spans":\[{.*"line_start":(?P<line>[0-9]+),"line_end":(?P<end_line>[0-9]+),"column_start":(?P<col>[0-9]+),"column_end":(?P<end_col>[0-9]+),.*"text":\[{"text":"(?P<near>.*)","highlight_start".*$'''
-    multiline = True
     defaults = {
         'selector': 'source.rust'
     }
-    on_stderr = None
+    error_stream = STREAM_STDERR
     name = 'rustc'
+    on_stderr = None
+    def find_errors(self, output):
+        for i in output.split('\n'):
+            error = json.loads(i)
+            long_message = error['message']
+            for child in error['children']:
+                long_message+="\n{}: {}".format(child['level'], child['message'])
+            yield LintMatch(
+                line = error['spans'][0]['line_start'],
+                end_line = error['spans'][0]['line_end'],
+                message = long_message,
+                col = error['spans'][0]['column_start'],
+                end_col = error['spans'][0]['column_end'],
+                error_type = error['level'],
+                near = error['spans'][0]['text'][0]['text'],
+                code = error['code']['code'],
+                filename = error['spans'][0]['file_name']
+            )

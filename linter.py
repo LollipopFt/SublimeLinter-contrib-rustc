a0bb72b89ce73f2893bab42a89140d96bd379570
LollipopFt <lollipopz1426@gmail.com>
2022-02-26 14:00:43 +0800

    first commit.

--- linter.py
+++ linter.py
@@ -1,132 +1,141 @@
 import json
+import os
+import sys
 import SublimeLinter.lint as SLlint  # Linter, LintMatch, STREAM_STDERR
 
 
 class Rustc(SLlint.Linter):
-    '''rustc linter'''
-    cmd = ('rustc', '--error-format=json', '--emit=mir', '-o', '/dev/null', '${file}')
-    defaults = {
-        'selector': 'source.rust'
-    }
-    error_stream = SLlint.STREAM_STDERR
-    name = 'rustc'
-    on_stderr = None
+  '''rustc linter'''
 
-    def find_errors(self, output):
-        '''function to find errors'''
-        lint_match = SLlint.LintMatch
+  def command(self):
+    '''check if rust enhanced can be used'''
+    if os.path.exists(os.path.join(self.working_dir, 'Cargo.toml')):
+      sys.exit()
 
-        def spanexpanse(msg, error, code, expanse, lint_match):
-            '''function to recurse ['expansion']'''
-            if expanse is not None:
-                spanes = expanse['span']
-                yield from spanexpanse(msg, error, code, spanes['expansion'], lint_match)
-                if (spanes['label'] is not None) & (spanes['label'] != ""):
-                    span_err_msg = msg+"\n{}: {}".format(spanes['suggested_replacement'], spanes['label'])
-                else:
-                    span_err_msg = msg
-                yield lint_match(
-                    line=spanes['line_start']-1,
-                    end_line=spanes['line_end']-1,
-                    message=span_err_msg,
-                    col=spanes['column_start']-1,
-                    end_col=spanes['column_end']-1,
-                    error_type=error['level'],
-                    code=code,
-                    filename=spanes['file_name']
-                )
-        for i in output.splitlines():
-            try:
-                error = json.loads(i)
-            except ValueError:
-                continue
-            if error['spans'] == []:
-                continue
-            if error['code'] is not None:
-                code = error['code']['code']
-            else:
+  cmd = (
+    'rustc', '--error-format=json', '--emit=mir', '-o', '/dev/null', '${file}'
+  )
+  defaults = {
+    'selector': 'source.rust'
+  }
+  error_stream = SLlint.STREAM_STDERR
+  name = 'rustc'
+  on_stderr = None
+
+  def find_errors(self, output):
+    '''function to find errors'''
+    lint_match = SLlint.LintMatch
+
+    for i in output.splitlines():
+      try:
+        compiled = json.loads(i)
+      except:
+        continue
+
+      mainmessage = compiled['message']
+
+      if compiled['code'] is None:
+        code = ''
+      else:
+        code = compiled['code']['code']
+
+      level = compiled['warning']
+
+      spans = compiled['spans']
+      if spans is None:
+        continue
+      for spansobj in spans:
+        msg = mainmessage
+        if spansobj['label'] is not None:
+          if spansobj['label'] != '':
+            msg += '\n' + spansobj['label']
+        if spansobj['suggested_replacement'] is not None:
+          if spansobj['suggested_replacement'] != '':
+            msg += '\n' + spansobj['suggested_replacement']
+        if spansobj['suggestion_applicability'] is not None:
+          if spansobj['suggestion_applicability'] != '':
+            msg += ' ('+spansobj['suggestion_applicability']+')'
+        yield lint_match(
+          filename=spansobj['file_name'],
+          line=spansobj['line_start']-1,
+          end_line=spansobj['line_end']-1,
+          col=spansobj['column_start']-1,
+          end_col=spansobj['column_end']-1,
+          error_type=level,
+          code=code,
+          message=msg
+        )
+
+        if spansobj['is_primary'] is True:
+          for child in compiled['children']:
+            if not child['spans']:
+              if child['code'] is None:
                 code = ''
-            for span in error['spans']:
-                msg = error['message']
-                if (span['label'] is not None) & (span['label'] != ""):
-                    err_msg = msg+"\n{}: {}".format(span['suggested_replacement'], span['label'])
-                else:
-                    err_msg = msg
-                yield lint_match(
-                    line=span['line_start']-1,
-                    end_line=span['line_end']-1,
-                    message=err_msg,
-                    col=span['column_start']-1,
-                    end_col=span['column_end']-1,
-                    error_type=error['level'],
-                    code=code,
-                    filename=span['file_name']
-                )
-                yield from spanexpanse(msg, error, code, span['expansion'], lint_match)
-                if span['expansion'] is not None:
-                    spanes = span['expansion']['span']
-                    if (spanes['label'] is not None) & (spanes['label'] != ""):
-                        span_err_msg = msg+"\n{}: {}".format(spanes['suggested_replacement'], spanes['label'])
-                    else:
-                        span_err_msg = msg
-                    yield lint_match(
-                        line=spanes['line_start']-1,
-                        end_line=spanes['line_end']-1,
-                        message=span_err_msg,
-                        col=spanes['column_start']-1,
-                        end_col=spanes['column_end']-1,
-                        error_type=error['level'],
-                        code=code,
-                        filename=spanes['file_name']
-                    )
-                for child in error['children']:
-                    if child['spans'] == []:
-                        if child['code'] is not None:
-                            code = child['code']
-                        else:
-                            code = ''
-                        yield lint_match(
-                            line=span['line_start']-1,
-                            end_line=span['line_end']-1,
-                            message=child['message'],
-                            col=span['column_start']-1,
-                            end_col=span['column_end']-1,
-                            error_type=child['level'],
-                            code=code,
-                            filename=span['file_name']
-                        )
-                        if span['expansion'] is not None:
-                            spanes = span['expansion']['span']
-                            yield lint_match(
-                                line=spanes['line_start']-1,
-                                end_line=spanes['line_end']-1,
-                                message=child['message'],
-                                col=spanes['column_start']-1,
-                                end_col=spanes['column_end']-1,
-                                error_type=child['level'],
-                                code=code,
-                                filename=spanes['file_name']
-                            )
-            if error['children'] == []:
-                continue
-            for child in error['children']:
-                if child['code'] is not None:
-                    code = child['code']
-                else:
-                    code = ''
-                if child['spans'] != []:
-                    for cspan in child['spans']:
-                        long_message = child['message']
-                        cspans = cspan['label']
-                        if (cspans is not None) & (cspans != ""):
-                            long_message += "\n{}: {}".format(cspan['suggested_replacement'], cspans)
-                        yield lint_match(
-                            line=cspan['line_start']-1,
-                            end_line=cspan['line_end']-1,
-                            message=long_message,
-                            col=cspan['column_start']-1,
-                            end_col=cspan['column_end']-1,
-                            error_type=child['level'],
-                            code=code,
-                            filename=cspan['file_name']
-                        )
+              else:
+                code = child['code']
+              yield lint_match(
+                filename=spansobj['file_name'],
+                line=spansobj['line_start']-1,
+                end_line=spansobj['line_end']-1,
+                col=spansobj['column_start']-1,
+                end_col=spansobj['column_end']-1,
+                error_type=child['level'],
+                code=code,
+                message=child['message']
+              )
+
+        if spansobj['expansion'] is None:
+          continue
+        for span in spansobj['expansion']['span']:
+          msg = mainmessage
+          if span['label'] is not None:
+            if span['label'] != '':
+              msg += '\n' + span['label']
+          if span['suggested_replacement'] is not None:
+            if span['suggested_replacement'] != '':
+              msg += '\n' + span['suggested_replacement']
+          if span['suggestion_applicability'] is not None:
+            if span['suggestion_applicability'] != '':
+              msg += ' ('+span['suggestion_applicability']+')'
+          yield lint_match(
+            filename=span['file_name'],
+            line=span['line_start']-1,
+            end_line=span['line_end']-1,
+            col=span['column_start']-1,
+            end_col=span['column_end']-1,
+            error_type=level,
+            code=code,
+            message=msg
+          )
+
+      for child in compiled['children']:
+        if child['spans']:
+          mainmessage = child['message']
+          level = child['level']
+          if child['code'] is None:
+            code = ''
+          else:
+            code = child['code']['code']
+          level = child['warning']
+          spans = child['spans']
+          for spans in child['spans']:
+            msg = mainmessage
+            if spans['label'] is not None:
+              if spans['label'] != '':
+                msg += '\n' + spans['label']
+            if spans['suggested_replacement'] is not None:
+              if spans['suggested_replacement'] != '':
+                msg += '\n' + spans['suggested_replacement']
+            if spans['suggestion_applicability'] is not None:
+              if spans['suggestion_applicability'] != '':
+                msg += ' ('+spans['suggestion_applicability']+')'
+            yield lint_match(
+              filename=spans['file_name'],
+              line=spans['line_start']-1,
+              end_line=spans['line_end']-1,
+              col=spans['column_start']-1,
+              end_col=spans['column_end']-1,
+              error_type=level,
+              code=code,
+              message=msg
+            )
